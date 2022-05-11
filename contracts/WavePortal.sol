c44613841c5adaad8571c46f064611bf7b0d79f7
Mikel Diaz <mikelmd@criptext.com>
2022-05-11 18:58:16 -0500

    We now store our data and made a few changes

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,17 +7,55 @@ import "hardhat/console.sol";
 contract WavePortal {
   uint256 totalWaves;
 
-  constructor() {
-    console.log("Yo yo, I am a smart contract but the dev is smarter");
-  }
+    //event creation to listen for the info of every new wave
+    event NewWave(address indexed from, uint256 timestamp, string message);
 
-  function wave() public {
-    totalWaves += 1;
-    console.log("%s has waved!", msg.sender);
-  }
+    //new structure creation named Wave. This is to customize how we store our data
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
 
-  function getTotalWaves() public view returns(uint256) {
-    console.log("We have %d total waves!", totalWaves);
-    return totalWaves;
-  }
+    /*
+     * I declare a variable waves that lets me store an array of structs.
+     * This is what lets me hold all the waves anyone ever sends to me!
+     */
+    Wave[] waves;
+
+    constructor() {
+        console.log("I AM SMART CONTRACT. POG.");
+    }
+
+    /*
+     * Changes on the previous wave function, now
+     * it requires a string called _message. This is the message our user
+     * sends us from the frontend!
+     */
+    function wave(string memory _message) public {
+        totalWaves += 1;
+        console.log("%s waved w/ message %s", msg.sender, _message);
+
+
+        //This is where I actually store the wave data in the array(Wave[]).
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         * I emit the 'NewWave' event so that the client can
+         * see the transaction at the front-end
+         */
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    /*
+     * I added a function getAllWaves which will return the struct array, waves, to us.
+     * This will make it easy to retrieve the waves from our website!
+     */
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        return totalWaves;
+    }
 }
